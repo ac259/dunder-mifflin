4b9187c125b9fdc1a4d1dce65d6a07105edef4b7
ac259 <ac259@buffalo.edu>
2025-03-12 22:30:23 -0400

    ollama >>>>>>> huggingface setup - need to figure out how there is so much difference

--- common/gemma_agent.py
+++ common/gemma_agent.py
@@ -1,8 +1,12 @@
-from transformers import pipeline, AutoTokenizer
+from ollama import chat
+from ollama import ChatResponse
 
-model_name = "google/gemma-3-1b-pt"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-pipe = pipeline("text-generation", model=model_name, tokenizer=tokenizer, device="mps", torch_dtype="auto")
-
-output = pipe("Eiffel tower is located in", max_new_tokens=50)
-print(output)
+response: ChatResponse = chat(model='gemma3:4b', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
+# or access fields directly from the response object
+print(response.message.content)
